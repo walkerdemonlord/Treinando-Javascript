86e49ce07d924daa93d9cb53c2912ff32608f6c7
Walker <166620151+walkerdemonlord@users.noreply.github.com>
2024-06-23 17:49:33 -0300

    Inicial

---
+++ script.js
@@ -0,0 +1,18 @@
+/* 
+Função: trecho de código que só é executado 
+quando é camado/invocado.
+
+    Função Void (vazia)
+    Função com Parâmetros (s)
+    Função Return
+    Função Arrow    
+
+*/
+
+let valor = 20;
+
+function incrementa () {
+    console.log("Estou dentro da função");
+}
+
+console.log(valor);
